3b5a916f8b30764ecef87e5c39f5287374b07398
SimoneZoroddu <lenviur@gmail.com>
2026-02-03 14:21:33 +0100

    create the cicle with for, counter. condition and increase

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // controllo che funziona il log
 
 console.log('salve');
+
+// funziona tutto inizio l esercizio con il primo punto
+
+for (let program = 1; program <= 100; program++) {
+    //controlliamo che funzioni correttamente il for
+    console.log(program);
+    
+}
