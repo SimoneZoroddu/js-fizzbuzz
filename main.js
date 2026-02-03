6c4211200b2b834f0a7f51083f39b877d8727420
SimoneZoroddu <lenviur@gmail.com>
2026-02-03 15:22:39 +0100

    Last commit with correction

--- main.js
+++ main.js
@@ -2,16 +2,22 @@
 
 console.log('salve');
 
+let name_1 = 'Fizz'
+let name_2 = 'Buzz'
+
 // funziona tutto inizio l esercizio con il primo punto
 
 for (let program = 1; program <= 100; program++) {
-    //controlliamo che funzioni correttamente il for
-    console.log(program);
-    if (program % 3 === 0) {
-        console.log('Fizz');
-    } else if (program % 5 == 0) {
-        console.log('Buzz');
-    } else if (program % 3 === 0 + program % 5 == 0) {
-        console.log('FizzBuzz');
-    }
+    let div_3 = (program % 3 == 0)
+    let div_5 = (program % 5 == 0)
+
+        if (div_3 && div_5) {
+            console.log(name_1 + name_2);
+        }   else if (div_3) {
+            console.log(name_1);
+        }   else if (div_5) {
+            console.log(name_2);
+        }   else {
+            console.log(program);
+        }
 }
